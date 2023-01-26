f83604b96fb949beb2ce1af2c56b8df878b054f5
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-26 12:23:35 +0700

    update file

--- tkcv2.py
+++ tkcv2.py
@@ -10,16 +10,13 @@ class Window(Tk):
         self.wm_title("form")
         self.geometry("550x420")
 
-        button_1 = Button(self,
-                          text="show tabel",
-                          command=self.show_frames)
+        # add button
+        button_1 = Button(self, text="show tabel", command=self.show_frames)
         button_1.place(x=50, y=350, width=100, height=50)
-
-        # show a label
-        self.label = Label(self,
-                           text='This is a label',
-                           font=('arial', 30, 'bold'))
+        # add a label
+        self.label = Label(self, text='This is a label', font=('arial', 30, 'bold'))
         self.label.place(x=80, y=20)
+
         # ===================== cv2  =======================
         self.cap = cv2.VideoCapture("video.mp4")
 
@@ -34,16 +31,16 @@ class Window(Tk):
         resized = cv2.resize(self.cap.read()[1], dim, interpolation=cv2.INTER_AREA)
         print('Resized Dimensions : ', resized.shape)
 
-        # Get the latest frame and convert into Image
+        # Get the latest frame and convert into PIL Image
         cv2image = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv2image)
 
-        # Convert image to PhotoImage
+        # Convert PIL image to photoImage
         imgtk = ImageTk.PhotoImage(image=img)
         self.label.imgtk = imgtk
         self.label.configure(image=imgtk)
 
-        # Repeat after an interval to capture continiously
+        # Repeat after an interval to capture continuously
         self.label.after(20, self.show_frames)
 
 
