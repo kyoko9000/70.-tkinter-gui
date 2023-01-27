422613806e143b1569875671ae60716fe69cffa6
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-27 11:16:13 +0700

    update file

--- tkcv2.py
+++ tkcv2.py
@@ -1,7 +1,8 @@
 from tkinter import Tk
-from tkinter.ttk import Label, Button
+from tkinter.ttk import Button, Label
+
 import cv2
-from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 
 
 class Window(Tk):
@@ -14,14 +15,14 @@ class Window(Tk):
         button_1 = Button(self, text="show tabel", command=self.show_frames)
         button_1.place(x=50, y=350, width=100, height=50)
         # add a label
-        self.label = Label(self, text='This is a label', font=('arial', 30, 'bold'))
+        self.label = Label(self, text='This is a label')
         self.label.place(x=80, y=20)
 
         # ===================== cv2  =======================
         self.cap = cv2.VideoCapture("video.mp4")
 
     def show_frames(self):
-        # resize image
+        # take width, height of image
         print('Original Dimensions : ', self.cap.read()[1].shape)
         scale_percent = 30  # percent of original size
         width = int(self.cap.read()[1].shape[1] * scale_percent / 100)
@@ -31,14 +32,14 @@ class Window(Tk):
         resized = cv2.resize(self.cap.read()[1], dim, interpolation=cv2.INTER_AREA)
         print('Resized Dimensions : ', resized.shape)
 
-        # Get the latest frame and convert into PIL Image
+        # convert cv2 image into PIL Image
         cv2image = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(cv2image)
 
-        # Convert PIL image to photoImage
-        imgtk = ImageTk.PhotoImage(image=img)
-        self.label.imgtk = imgtk
-        self.label.configure(image=imgtk)
+        # Convert PIL image to tkinter image
+        tkimage = ImageTk.PhotoImage(image=img)
+        self.label.imgtk = tkimage
+        self.label.configure(image=tkimage)
 
         # Repeat after an interval to capture continuously
         self.label.after(20, self.show_frames)
