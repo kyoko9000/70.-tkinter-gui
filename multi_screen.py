3897eba9b5d1caca3f6ba90d7c8ead2de6ac5bb1
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-29 12:52:26 +0700

    update file

--- multi_screen.py
+++ multi_screen.py
@@ -3,8 +3,8 @@ from tkinter.ttk import Button, Label
 
 
 class Window_1(Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
 
         self.geometry('300x200')
         self.title('Toplevel Window')
@@ -12,17 +12,10 @@ class Window_1(Toplevel):
         self.label = Label(self, text="hi")
         self.label.pack(expand=True)
 
-        self.button = Button(self,
-                             text='Open a window',
-                             command=self.main_control)
+        self.button = Button(self, text='sub control', command=self.sub_control)
         self.button.place(x=50, y=150)
 
-        self.button_1 = Button(self,
-                               text='Open a window',
-                               command=self.main_control)
-        self.button_1.place(x=50, y=150)
-
-    def main_control(self):
+    def sub_control(self):
         self.label.config(text="lam choi")
 
 
@@ -37,27 +30,23 @@ class Window(Tk):
         self.label.pack(expand=True)
 
         # place a button on the root window
-        self.button = Button(self,
-                             text='Open a window',
-                             command=self.open_window)
+        self.button = Button(self, text='Open a window', command=self.open_window)
         self.button.place(x=50, y=150)
 
         # place a button on the root window
-        button_1 = Button(self,
-                          text='set text',
-                          command=self.set_text)
+        button_1 = Button(self, text='main_control', command=self.main_control)
         button_1.place(x=200, y=150)
 
-    def set_text(self):
+    def open_window(self):
+        self.window_1 = Window_1()
+        # self.window_1.grab_set()
+        self.window_1.button.configure(command=self.sub_control)
+
+    def main_control(self):
         self.label.config(text="main control")
         self.window_1.label.config(text="main control")
 
-    def open_window(self):
-        self.window_1 = Window_1(self)
-        # self.window.grab_set()
-        self.window_1.button_1.configure(command=self.hh)
-
-    def hh(self):
+    def sub_control(self):
         self.label.config(text="sub control")
         self.window_1.label.config(text="sub control")
 
